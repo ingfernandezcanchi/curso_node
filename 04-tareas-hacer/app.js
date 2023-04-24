c55063d3ec6813ae3b73a5579ee42ae0467a1d44
Hugo Fernandez <hugo.j.fernandez@accenture.com>
2023-04-24 02:06:56 -0300

    Section 5.50

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,18 @@
+require('colors');
+//const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const {inquirerMenu, pausa} = require('./helpers/inquirer');
+console.clear();
+
+const main = async()=>{
+    console.log('Hola Mundo');
+    let opt = '';
+    do{
+        //opt = await mostrarMenu();
+        opt = await inquirerMenu();
+        console.log({ opt });
+        if (opt !== '0') await pausa();
+    }while(opt !== '0');
+    //pausa();
+}
+
+main();
